4c05b432dccd7ac743d648d16cf7136da0c3f96a
Michael Ashton <mykl.ashton@gmail.com>
2017-11-04 17:09:11 -0400

    Add potential interval clearing for multiple users

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 const PORT = process.env.PORT || 8000;
 moment().format();
 
+
 io.on('connection', (client) => {
   // this is where you emitt events to the client
   client.on('subscribeToTimer', (interval) => { 
@@ -11,7 +12,7 @@ io.on('connection', (client) => {
     console.log('client is subscribing to timer with interval', interval);
     setInterval( () => { 
       // when timer fires (at interval of clients choosing), emit event.
-      client.emit('timer', moment()); // first argument of emit is name of event, second is the content
+      client.emit('timer', moment().format()); // first argument of emit is name of event, second is the content
     }, interval)
   })
 })
@@ -19,3 +20,24 @@ io.on('connection', (client) => {
 // tell socket.io where to start listening for clients
 io.listen(PORT);
 console.log("Socket listening on port", PORT);
+
+
+// Do I need to clear the interval for each user?
+/*
+let interval;
+io.on("connection", (client) => {
+
+  console.log("New client connected");
+  
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  interval = setInterval( () => subscribeToTimer(client), interval);
+
+  client.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+*/
