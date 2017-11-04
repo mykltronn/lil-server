006ca04c07085b653a304b5729d871642db68064
Michael Ashton <mykl.ashton@gmail.com>
2017-11-04 15:55:04 -0400

    Basic socket server runs

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+const io = require('socket.io')();
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const PORT = process.env.PORT || 8000;
 
-const server = express() // create http server
-  .use( (req, res) => res.sendFile(INDEX) ) // send client index
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`)); // listen local host or server port
+io.on('connection', (client) => {
+  // this is where you emitt events to the client
+  client.on('subscribeToTimer', (interval) => { 
+    // when client initializes the 'subscribeToTimer' socket, initiate socket. Client will pass the argument 'interval' 
+    console.log('client is subscribing to timer with interval', interval);
+    setInterval( () => { 
+      // when timer fires (at interval of clients choosing), emit event.
+      client.emit('timer', new Date()); // first argument of emit is name of event, second is the content
+    }, interval)
+  })
+})
 
-const wss = new SocketServer({ server }); // instantiate new websocket server with http server as argument to listen for upgrade events
-
-wss.on('connection', (ws) => { // listen for connection
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected')); // listen for disconnection
-});
-
-
-// one benefit sockets is that you can push data to a client without waiting for a client request
-setInterval(() => { // push the current time to client every second
-  wss.clients.forEach( (client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+// tell socket.io where to start listening for clients
+io.listen(PORT);
+console.log("Socket listening on port", PORT);
